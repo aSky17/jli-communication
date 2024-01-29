cbf792c3cbc3b3376c70807580fd9da836ec0b05
aSky17 <akashmishra.am.1710@gmail.com>
2024-01-29 22:50:01 +0530

    Added springboot configuration for websockets

---
+++ src/main/java/com/jli/jlicommunications/JliCommunicationsApplication.java
@@ -0,0 +1,13 @@
+package com.jli.jlicommunications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JliCommunicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JliCommunicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jli/jlicommunications/JliCommunicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jli.jlicommunications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JliCommunicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
